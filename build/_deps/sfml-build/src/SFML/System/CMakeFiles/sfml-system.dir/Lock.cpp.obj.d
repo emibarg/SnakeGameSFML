d3d9564bb4c50c0a5b47b3c7eed1710ec30f421f
emib <emilianobarg@gmail.com>
2023-11-23 21:05:30 -0300

    Revert "installer"
    This reverts commit 6c5951f23b4bed8d9b0e34195911522a0202c7ca.

--- build/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.obj.d
+++
@@ -1,7 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.obj: \
- C:\Users\emiba\Desktop\SnakeGameSFML\build\_deps\sfml-src\src\SFML\System\Lock.cpp \
- C:/Users/emiba/Desktop/SnakeGameSFML/build/_deps/sfml-src/include/SFML/System/Lock.hpp \
- C:/Users/emiba/Desktop/SnakeGameSFML/build/_deps/sfml-src/include/SFML/System/Export.hpp \
- C:/Users/emiba/Desktop/SnakeGameSFML/build/_deps/sfml-src/include/SFML/Config.hpp \
- C:/Users/emiba/Desktop/SnakeGameSFML/build/_deps/sfml-src/include/SFML/System/NonCopyable.hpp \
- C:/Users/emiba/Desktop/SnakeGameSFML/build/_deps/sfml-src/include/SFML/System/Mutex.hpp
